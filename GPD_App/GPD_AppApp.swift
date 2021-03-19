d8bd111c1d00dbbf417cb7fffcab3717e1b61925
pedro <pfraguas@udesa.edu.ar>
2021-03-19 20:14:11 -0300

    project creation

---
+++ GPD_App/GPD_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPD_AppApp.swift
+//  GPD_App
+//
+//  Created by Pedro Fraguas on 19/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct GPD_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
